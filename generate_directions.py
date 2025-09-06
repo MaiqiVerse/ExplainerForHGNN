df26eaaeb0a8a8af973b1e5fb1fd5f0cbc84a4e5
Armiiiiin <yiran28248036@gmail.com>
2025-09-06 22:04:26 +0000

    PGE debugged again

---
+++ generate_directions.py
@@ -0,0 +1,4 @@
+from utils import edge_direction_creation
+
+edge_direction_creation.process_node_classification_dataset_to_edge_directions(dataset_path={"Your dataset"},
+                                                                               label_path={"Your labels.pkl file"})
